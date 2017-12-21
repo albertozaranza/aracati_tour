0622c20e1f53ff544d584311dde84a1cffd13782
albertozaranza <albertozaranza@outlook.com>
2017-12-21 10:09:41 -0300

    Tratamento de negação de permissão.

--- app/src/main/java/br/edu/ifce/aracatitour/activity/MainActivity.java
+++ app/src/main/java/br/edu/ifce/aracatitour/activity/MainActivity.java
@@ -1,9 +1,12 @@
 package br.edu.ifce.aracatitour.activity;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -85,6 +88,27 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), "Criando banco", Toast.LENGTH_SHORT).show();
             manageDatabase.addItem();
             Toast.makeText(getApplicationContext(), "Banco criado", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        for(int resultado : grantResults){
+            if(resultado == PackageManager.PERMISSION_DENIED){
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Permissões negadas");
+                builder.setMessage("Para utilizar esse app, é necessário aceitar as permissões");
+                builder.setPositiveButton("CONFIRMAR", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                });
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
         }
     }
 }
